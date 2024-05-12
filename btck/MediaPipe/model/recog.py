ea9f02a6b125862645dfd4fa9f115ebcd30fc015
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-05-12 17:16:47 +0700

    version1

--- btck/MediaPipe/model/recog.py
+++ btck/MediaPipe/model/recog.py
@@ -7,6 +7,20 @@ import time
 import mediapipe as mp
 import math
 from tensorflow.keras.models import load_model
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="geomodellingck"
+)
+cursor = conn.cursor()
+
+def save_action(name):
+    insert_query = "INSERT INTO actions (name) VALUES (%s)"
+    cursor.execute(insert_query, (name,))  
+    conn.commit()
 
 # chay 3
 def mediapipe_detection(image, model):
@@ -18,14 +32,14 @@ def mediapipe_detection(image, model):
     return image, results
 
 # chay 4
-async def draw_landmarks(image, results):
+def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS) # Draw face connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
 
 # chay 5
-async def draw_styled_landmarks(image, results):
+def draw_styled_landmarks(image, results):
     # Draw pose connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
                              mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
@@ -81,92 +95,78 @@ old_time = time.time()
 cap = cv2.VideoCapture(0)
 # Set mediapipe model
 
-async def main(websocket, path):
-    sequence = []
-    sentence = []
-    texts = []
-    threshold = 0.95
-    is_save = True
-    old_time = time.time()
-    cap = cv2.VideoCapture(0)
+# Khi bắt đầu thì xóa toàn bộ dữ liệu (làm sạch)
+delete_query = "DELETE FROM actions"
+cursor.execute(delete_query)
+conn.commit()
+# Khi bắt đầu thì xóa toàn bộ dữ liệu (làm sạch)
 
-    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        while cap.isOpened():
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
 
-            # Read feed
-            ret, frame = cap.read()
+        # Read feed
+        ret, frame = cap.read()
 
-            # Make detections
-            image, results = mediapipe_detection(frame, holistic)
-            print(results)
-            if (not results.left_hand_landmarks) and (not results.right_hand_landmarks):
-                is_save = False
-            # if (results.left_hand_landmarks):
-            #     print(results.left_hand_landmarks)
-            # if (results.right_hand_landmarks):
-            #     print(results.right_hand_landmarks)
-            # Draw landmarks
-            draw_styled_landmarks(image, results)
-            
-            if is_save:
-                # 2. Prediction logic
-                keypoints = extract_keypoints_reduce(results)
-                sequence.append(keypoints)
-                sequence = sequence[-30:]
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        print(results)
+        if (not results.left_hand_landmarks) and (not results.right_hand_landmarks):
+            is_save = False
+        # if (results.left_hand_landmarks):
+        #     print(results.left_hand_landmarks)
+        # if (results.right_hand_landmarks):
+        #     print(results.right_hand_landmarks)
+        # Draw landmarks
+        draw_styled_landmarks(image, results)
+        
+        if is_save:
+            # 2. Prediction logic
+            keypoints = extract_keypoints_reduce(results)
+            sequence.append(keypoints)
+            sequence = sequence[-30:]
 
-                if len(sequence) == 30:
-                    res = model.predict(np.expand_dims(sequence, axis=0))[0]
-                    print(len(res))
-                    print(actions[np.argmax(res)])
+            if len(sequence) == 30:
+                res = model.predict(np.expand_dims(sequence, axis=0))[0]
+                print(len(res))
+                print(actions[np.argmax(res)])
 
-                #3. Viz logic
-                    if res[np.argmax(res)] > threshold:
-                        if len(sentence) > 0:
-                            if actions[np.argmax(res)] == sentence[-1]:
-                                sentence.append(actions[np.argmax(res)])
-                        else:
+            #3. Viz logic
+                if res[np.argmax(res)] > threshold:
+                    if len(sentence) > 0:
+                        if actions[np.argmax(res)] == sentence[-1]:
                             sentence.append(actions[np.argmax(res)])
                     else:
-                        sentence = []
+                        sentence.append(actions[np.argmax(res)])
+                else:
+                    sentence = []
 
-                    if len(sentence) > 5:
-                        if len(texts) > 0:
-                            if sentence[-1] != texts[-1]:
-                                texts.append(sentence[-1])
-                        else:
+                if len(sentence) > 5:
+                    if len(texts) > 0:
+                        if sentence[-1] != texts[-1]:
                             texts.append(sentence[-1])
-                        # ghi texts[-1] vào dòng tiếp theo của file txt
-                        new_time = time.time()
-                        duration = new_time - old_time
-                        old_time = new_time
-                        if (duration > 2):
-                            print("ACTION : ", texts[-1])
-                            await websocket.send(texts[-1])
-                            await asyncio.sleep(1)
-                            # with open('actions.txt', 'a') as f:
-                            #     f.write(texts[-1] + '\n')
-                        if len(texts) > 3:
-                            texts = texts[-3:]
-                        sentence = sentence[-5:]
+                    else:
+                        texts.append(sentence[-1])
+                    # ghi texts[-1] vào dòng tiếp theo của file txt
+                    new_time = time.time()
+                    duration = new_time - old_time
+                    old_time = new_time
+                    if (duration > 2):
+                        save_action(texts[-1])
+                        # with open('actions.txt', 'a') as f:
+                            # f.write(texts[-1] + '\n')
+                    if len(texts) > 3:
+                        texts = texts[-3:]
+                    sentence = sentence[-5:]
 
-            cv2.putText(image, ' '.join(texts), (3,30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(image, ' '.join(texts), (3,30),
+                       cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
 
-            # Show to screen
-            cv2.imshow('OpenCV Feed', image)
-            is_save = True
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+        is_save = True
 
-            # Break gracefully
-            if cv2.waitKey(10) & 0xFF == ord('q'):
-                break
-        cap.release()
-        cv2.destroyAllWindows()
-
-import asyncio
-import websockets
-import random
-
-start_server = websockets.serve(main, "localhost", 8765)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
--- btck/MediaPipe/model/server.py
+++ btck/MediaPipe/model/server.py
@@ -1,18 +1,43 @@
 import asyncio
 import websockets
 import random
+import mysql.connector
+
+
 
 async def send_random_value(websocket, path):
     while True:
-        await asyncio.sleep(1)
-        with open('actions.txt', 'r') as file:
-            lines = file.readlines()
-        action = lines[0].strip() 
-        await asyncio.sleep(1)
-        with open('actions.txt', 'w') as file:
-            file.writelines(lines[1:])
-        print(action)
-        await websocket.send(str(action))
+
+        # Kết nối đến cơ sở dữ liệu
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="geomodellingck"
+        )
+
+        # Tạo con trỏ để thao tác với cơ sở dữ liệu
+        cursor = conn.cursor()
+
+        # Thực hiện câu lệnh SQL để lấy id và name của dòng có id lớn nhất trong bảng actions
+        select_query = "SELECT id, name FROM actions ORDER BY id ASC LIMIT 1"
+        cursor.execute(select_query)
+        result = cursor.fetchone()
+        print(result)
+
+        if result:
+            id, name = result
+            print(name)
+            await websocket.send(str(name))
+
+            # Xóa dòng này bằng id
+            delete_query = "DELETE FROM actions WHERE id = %s"
+            cursor.execute(delete_query, (id,))  # Chèn id vào câu lệnh DELETE
+            conn.commit()
+        else:
+            # Nếu không có dữ liệu trong bảng actions, gửi 'something' qua websocket
+            await websocket.send(str('something'))
+
         await asyncio.sleep(1)
 
 start_server = websockets.serve(send_random_value, "localhost", 8765)
