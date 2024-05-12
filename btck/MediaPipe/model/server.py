c62814c07483f21f4311a7755188c105e24f9b6d
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-05-12 12:16:30 +0700

    backup2

--- btck/MediaPipe/model/server.py
+++ btck/MediaPipe/model/server.py
@@ -3,12 +3,16 @@ import websockets
 import random
 
 async def send_random_value(websocket, path):
-    with open('actions.txt', 'r') as file:
-        actions = file.readlines()
-    
-    while actions:
-        action = actions.pop(0).strip()  # Get the first action and remove it from the list
-        await websocket.send(action)
+    while True:
+        await asyncio.sleep(1)
+        with open('actions.txt', 'r') as file:
+            lines = file.readlines()
+        action = lines[0].strip() 
+        await asyncio.sleep(1)
+        with open('actions.txt', 'w') as file:
+            file.writelines(lines[1:])
+        print(action)
+        await websocket.send(str(action))
         await asyncio.sleep(1)
 
 start_server = websockets.serve(send_random_value, "localhost", 8765)
