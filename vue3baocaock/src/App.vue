446c3cff6548ee85d12ab0f3694bdb1eb93c8a73
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-05-14 23:46:15 +0700

    three - leaflet

--- vue3baocaock/src/App.vue
+++ vue3baocaock/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <LeafletComp />
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import LeafletComp from './components/LeafletComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    LeafletComp
   }
 }
 </script>
--- vue3baocaock/src/main.js
+++ vue3baocaock/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 const app = createApp(App);
+import * as THREE from 'three';
+import 'leaflet/dist/leaflet.css';
+app.use(THREE)
 app.mount('#app')
