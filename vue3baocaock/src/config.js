169fb212242f4f2c56012a709a8375192fc6c4b0
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-05-16 00:55:47 +0700

    init vue3 template

--- vue3baocaock/src/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-	// API_URL: 'https://linebotbe-production.up.railway.app/api/',
-	// URL: 'https://linebotbe-production.up.railway.app/'
-	API_URL: 'http://localhost:99/api/',
-	URL: 'http://localhost:99/'
-}
