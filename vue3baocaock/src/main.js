8c2417d8f5d9fdb5d93fa4e908829c7733e07871
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-05-16 00:39:21 +0700

    refactor vue3baocaock

--- vue3baocaock/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-const app = createApp(App);
-import * as THREE from 'three';
-import 'leaflet/dist/leaflet.css';
-app.use(THREE)
-app.mount('#app')
