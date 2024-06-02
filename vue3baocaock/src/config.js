f99540eb611c83cb1f8c1a2017475f4c418afbdc
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-06-02 14:39:35 +0700

    config ngrok

--- vue3baocaock/src/config.js
+++ vue3baocaock/src/config.js
@@ -1,7 +1,11 @@
 export default {
-	API_URL: 'http://localhost:8000/api/',
-	URL: 'http://localhost:8000/',
-	API_Model: 'http://127.0.0.1:8000/'
+	// API_URL: 'http://localhost:8000/api/',
+	// URL: 'http://localhost:8000/',
+	// API_Model: 'http://127.0.0.1:8000/'
+	API_URL: 'https://4dc5-2405-4802-95f1-da5b-b0a5-b9d7-7e40-9350.ngrok-free.app/api/',
+	URL: 'https://4dc5-2405-4802-95f1-da5b-b0a5-b9d7-7e40-9350.ngrok-free.app/',
+	API_Model: 'https://4dc5-2405-4802-95f1-da5b-b0a5-b9d7-7e40-9350.ngrok-free.app/'
+	https://4dc5-2405-4802-95f1-da5b-b0a5-b9d7-7e40-9350.ngrok-free.app
 }
 
 // php artisan serve --host=127.0.0.1 --port=8089 // localhost 
